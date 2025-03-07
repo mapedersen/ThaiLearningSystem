50cd4762d37d4974b4c86b0d0cb4dfdd174f025f
marcus pedersen <marcus@mpedersen.me>
2025-03-08 00:53:56 +0100

    remove YouTubePlayer component and clean up App.tsx

--- WebUI/src/App.tsx
+++ WebUI/src/App.tsx
@@ -1,11 +1,5 @@
-import Player from "./components/Player";
-
 const App = () => {
-  return (
-    <div>
-      <Player videoId="2aTQVe_Y4So" />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
